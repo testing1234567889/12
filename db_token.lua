6e40bb49b2d75e2a3ab1588c3f5af81972cfc727
Orang <ujicobapertamax1@gmail.com>
2026-03-18 00:51:45 +0800

    Update db_token.lua

--- db_token.lua
+++ db_token.lua
@@ -1,6 +1,5 @@
 return {
-    ["TEST-1"] = {year=2025, month=1, day=1},
-    ["VIP-A"]  = {year=2026, month=6, day=15},
-    ["VIP-B"]  = {year=2026, month=12, day=31},
-    ["LIFETIME"] = {year=2099, month=1, day=1}
+    ["VIP-1"] = {year=2026, month=12, day=31, hwid="NONE"},
+    
+    ["VIP-A"]  = {year=2026, month=12, day=31, hwid="HWID_V11,LUA"},
 }
