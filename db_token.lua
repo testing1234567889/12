9cf70c8e8d55cf1ac1e99f1dc60a7b3d919bebcd
Orang <ujicobapertamax1@gmail.com>
2026-03-17 23:49:09 +0800

    Create db_token.lua

---
+++ db_token.lua
@@ -0,0 +1,6 @@
+return {
+    ["TEST-1"] = {year=2025, month=1, day=1},
+    ["VIP-A"]  = {year=2026, month=6, day=15},
+    ["VIP-B"]  = {year=2026, month=12, day=31},
+    ["LIFETIME"] = {year=2099, month=1, day=1}
+}
